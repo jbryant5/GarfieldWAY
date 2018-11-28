2253456a66632389f0302407598cfedf7d892e5b
Ing <GAR_thing@seattleschools.org>
2018-11-28 10:25:51 -0800

    Starting work

---
+++ pins/models.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+class Pin(models.Model):
+   pin_name = models.CharField(max_length=100)
+   pin_description = models.CharField(max_length=300)
+   pin_room = models.CharField(max
+   pub_date = models.DateTimeField('date published')
+   
+class Vote(models.Model):
+   pin = models.ForeignKey(Pin, on_delete=models.CASCADE)
+   vote_text = models.CharField(max_length=10)
+   votes = models.IntegerField(default=0)
