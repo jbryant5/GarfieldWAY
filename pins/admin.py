a06746efb83bdf8baa4457358dbb2c511828e0ee
Trevor Ing <trevfix@gmail.com>
2018-12-05 09:31:42 -0800

    misc. changes

--- pins/admin.py
+++ pins/admin.py
@@ -4,3 +4,10 @@ from __future__ import unicode_literals
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+
+from .models import Pin
+from .models import Vote
+
+admin.site.register(Vote)
+admin.site.register(Pin)
