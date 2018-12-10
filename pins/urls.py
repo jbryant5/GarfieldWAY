70201b57d70694cdac19bd31707e43073e76109b
Micah Tratt <micahtratt@gmail.com>
2018-12-10 09:44:33 -0800

    Pin test with number of pin counter

--- pins/urls.py
+++ pins/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
    url(r'^$', views.index, name='index'),
    url('create/', views.create, name='create'),
+   url('test/', views.test, name= 'test'),
+   url('getallrooms/', views.getAllRoomPins, name = 'number of pins: ')
 ]   
--- pins/views.py
+++ pins/views.py
@@ -24,3 +24,19 @@ def create(request):
         'latest_pin_list': latest_pin_list,
     }
     return HttpResponse(template.render(context, request))
+
+def test(request):
+    test_pin = Pin ()
+    test_pin.pin_name = 'test pin!'
+    test_pin.pin_room = 105
+    test_pin.pin_description = 'this is a test pin'
+    test_pin.pub_date = '2018-12-10 09:23:00'
+    test_pin.save()
+    return HttpResponse ('Test pin')
+
+def getAllRoomPins (request):
+    numberOfPins = len(Pin.objects.filter(pin_room = request.GET.get('room')))
+    return HttpResponse("Number of Pins: " + str(numberOfPins))
+
+
+
