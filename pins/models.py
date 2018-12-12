db315252811b80ee4c29c5191fe1e00ccaf1df56
Jack Bryant <GAR_jabryant@seattleschools.org>
2018-12-12 10:30:59 -0800

    adding different types for events

--- pins/models.py
+++ pins/models.py
@@ -2,6 +2,12 @@
 from __future__ import unicode_literals
 
 from django.db import models
+TYPE_CHOICES = (
+    ('c', 'Club'),
+    ('p', 'Public'),
+    ('r', 'Private'),
+    ('o', 'Other'),
+)
 
 # Create your models here.
 class Pin(models.Model):
@@ -11,10 +17,11 @@ class Pin(models.Model):
    pub_date = models.DateTimeField('date published')
    def __str__(self):
       return self.pin_name
-   
+   type = models.CharField(max_length=1, choices=TYPE_CHOICES)
 class Vote(models.Model):
    pin = models.ForeignKey(Pin, on_delete=models.CASCADE)
    vote_text = models.CharField(max_length=10)
    votes = models.IntegerField(default=0)
    def __str__(self):
       return self.vote_text
+      
