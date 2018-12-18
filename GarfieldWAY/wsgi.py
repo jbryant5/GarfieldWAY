ca961c288773ceee82472880bc5b1e723a4b65c5
Tyler Menezes <tylermenezes@gmail.com>
2018-12-18 11:55:45 -0800

    wsgi -> middleware

--- GarfieldWAY/wsgi.py
+++ GarfieldWAY/wsgi.py
@@ -1,7 +1,5 @@
 import os
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
 
 os.environ["DJANGO_SETTINGS_MODULE"] = "GarfieldWAY.settings"
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
