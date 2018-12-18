ac2acaa18524d12d2c641e3f6fe60ccb759f761f
Ing <GAR_thing@seattleschools.org>
2018-12-18 09:45:03 -0800

    redirect added to create screen

--- GarfieldWAY/settings.py
+++ GarfieldWAY/settings.py
@@ -81,6 +81,18 @@ DATABASES = {
     }
 }
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.mysql',
+#        'NAME': 'ghsway',
+#        'USER': 'ghsway',
+#        'PASSWORD': 'ghsbulldogs',
+#        'HOST': '107.170.211.181',
+#        'PORT': '3306',
+#    }
+#}
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
--- pins/views.py
+++ pins/views.py
@@ -6,6 +6,7 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 from django.template import loader
+from django.shortcuts import redirect
 
 from .models import Pin, Vote
 
@@ -35,7 +36,10 @@ def create(request):
        pin.date = request.POST.get('date')
        pin.pin_type = request.POST.get('pin_type')
        pin.save()
-       return HttpResponse(index(request))
+       if request.POST.get('_save') is not None:
+         return redirect('index')
+       else:
+         return redirect('create')
 
 def clear(request):
     Pin.objects.all().delete()
