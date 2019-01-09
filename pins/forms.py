396e393e5ad482ebfbc31623309defa9f32c1064
Micah Tratt <micahtratt@gmail.com>
2019-01-09 10:15:51 -0800

    corrected imports for pins.forms.py

--- pins/forms.py
+++ pins/forms.py
@@ -1,11 +1,6 @@
 from django import forms
-<<<<<<< HEAD
-
-from .models import Pin
-=======
 from .models import Pin
 from django.db import models
->>>>>>> micah
 from django.contrib.admin import widgets
 
 
@@ -17,17 +12,10 @@ class PinForm(forms.ModelForm):
             'pin_room': 'Room: ',
             'pin_description': 'Description',
             'pin_type': 'Type',
-<<<<<<< HEAD
-        }
-        help_texts = {
-            'date': 'YYYY-MM-DD HH:MM:SS',
-            'pin_room': '100-140 or 200-240 or 300-340'
-=======
             'date': 'Date',
         }
         help_texts = {
             'date': 'YYYY-MM-DD HH:MM:SS',
             'pin_room': '100-140 or 200-240 or 300-340',
->>>>>>> micah
             'pin_description': 'please keep descriptions concise',
         }
