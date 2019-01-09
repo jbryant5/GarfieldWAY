ec1c2d3a7fb9cf52b66021ef30fdc826c9d01a44
Micah Tratt <micahtratt@gmail.com>
2019-01-09 10:04:22 -0800

    working forms for pins

---
+++ pins/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+from .models import Pin
+from django.db import models
+from django.contrib.admin import widgets
+
+
+class PinForm(forms.ModelForm):
+    class Meta:
+        model = Pin
+        exclude = ('pub_date',)
+        labels = {
+            'pin_room': 'Room: ',
+            'pin_description': 'Description',
+            'pin_type': 'Type',
+            'date': 'Date',
+        }
+        help_texts = {
+            'date': 'YYYY-MM-DD HH:MM:SS',
+            'pin_room': '100-140 or 200-240 or 300-340',
+            'pin_description': 'please keep descriptions concise',
+        }
