c0aeb82d2f313c3afc88572fc503446382d47c02
Ing <GAR_thing@seattleschools.org>
2019-01-22 09:48:21 -0800

    finishing edit screen

--- pins/urls.py
+++ pins/urls.py
@@ -1,5 +1,4 @@
 from django.conf.urls import url
-from django.urls import path
 
 from . import views
 
@@ -10,6 +9,6 @@ urlpatterns = [
    url('create/', views.create, name='create'),
    url('clear/', views.clear, name= 'clear'),
    url('test/', views.test, name= 'test'),
-   path('edit/<int:pin_id>', views.edit, name= 'edit'),
+   url(r'^edit/(?P<pin_id>[0-9]+)/$', views.edit, name= 'edit'),
    url('getallrooms/', views.getAllRoomPins, name = 'number of pins ')
 ]   
