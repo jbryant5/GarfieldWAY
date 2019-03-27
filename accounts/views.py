98131e7b11fb1886ef629623799a930ec00c0ac3
Ing <GAR_thing@seattleschools.org>
2019-03-27 09:31:24 -0700

    connecting links to sign up features

--- accounts/views.py
+++ accounts/views.py
@@ -19,9 +19,9 @@ def signup(request):
             user.refresh_from_db()  # load the profile instance created by the signal
             user.save()
             raw_password = form.cleaned_data.get('password')
-            user = authenticate(username=user.username, password=raw_password)
+           # user = authenticate(username=user.username, password=raw_password)
             login(request, user)
-            return redirect('home')
+            return redirect('/pins')
     else:
         form = UserCreationForm()
     return render(request, 'signup.html', {'form': form})
