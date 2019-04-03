c2d43c75ce477fb31294175acc28788c70d8c81d
Ing <GAR_thing@seattleschools.org>
2019-04-03 10:20:23 -0700

    adding password change stuff

--- accounts/urls.py
+++ accounts/urls.py
@@ -23,5 +23,6 @@ urlpatterns = [
     url(r'^signup/$', views.signup, name='signup'),
     url(r'^login/$', auth_views.LoginView.as_view(), name='login'),
     url(r'^logout/$', auth_views.LogoutView.as_view(), name='logout'),
+    url(r'^password_change/$', auth_views.PasswordChangeView.as_view(), name='password_change'),
 
 ]
